8226fe58af60d3669e8276fddad5764903d10c24
lifecodeof <m.semihkoyuncu01@gmail.com>
2022-07-20 17:32:16 +0300

    added ability to export

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,12 +4,11 @@ import React, { FC } from "react"
 export type Project = { title: string, desc: string, link: string, image: string | StaticImageData }
 export const ProjectCard: FC<Project> = ({ title, desc, link, image }: Project) => {
     return (
-        <a className="p-2 rounded-md
-        border border-gray-400 hover:border-blue-600
-        [&_h2]:hover:text-blue-600" href={link} target="_blank">
-                <Image className="mb-3 rounded-md" src={image} alt={title} />
-                <h2>{title} &#10230;</h2>
-                <p className="text-gray-400">{desc}</p>
+        <a className="p-2 rounded-md border border-gray-400 hover:border-blue-600[&_h2]:hover:text-blue-600"
+            href={link} target="_blank" rel="noreferrer">
+            <Image className="mb-3 rounded-md" src={image} alt={title} />
+            <h2>{title} &#10230;</h2>
+            <p className="text-gray-400">{desc}</p>
         </a>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
         <section id="projects">
           <h1>Projects</h1>
           <div className="mt-3 mx-4 lg:mx-auto lg:w-2/3 grid grid-cols-2 gap-3">
-            {projects.map(p => <ProjectCard {...p} />)}
+            {projects.map(p => <ProjectCard {...p} key={p.title} />)}
           </div>
         </section>
       </main>
@@ -39,8 +39,8 @@ const Home: NextPage = () => {
       <footer className='m-4 flex justify-around'>
         <p>Semih Koyuncu</p>
         <div className='flex [&>*]:mx-1'>
-          <a href="https://github.com/lifecodeof"><Image src={github} width={32} height={32}/></a>
-          <a href="https://linkedin.com"><Image src={linkedin} width={32} height={32}/></a>
+          <a href="https://github.com/lifecodeof"><Image src={github} alt="github" width={32} height={32}/></a>
+          <a href="https://linkedin.com"><Image src={linkedin} alt="linkedin" width={32} height={32}/></a>
         </div>
       </footer>
     </>
