8faf62119ff4005da252582ef25452995ce7de58
lifecodeof <m.semihkoyuncu01@gmail.com>
2022-07-21 19:07:36 +0300

    fix contrast on light

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,7 +4,7 @@ import React, { FC } from "react"
 export type Project = { title: string, desc: string, link: string, image: string | StaticImageData }
 export const ProjectCard: FC<Project> = ({ title, desc, link, image }: Project) => {
     return (
-        <article className="group relative border border-gray-400">
+        <article className="group relative border border-gray-500 dark:border-gray-400">
             <Image layout="responsive" src={image} alt={title} />
             <div className="absolute inset-0 z-10 bg-white dark:bg-black
             bg-opacity-0 group-hover:bg-opacity-80 touch:bg-opacity-80
@@ -13,10 +13,10 @@ export const ProjectCard: FC<Project> = ({ title, desc, link, image }: Project) 
             " />
             <a className="absolute inset-0 ml-4 mt-3 z-20
                 transition-all duration-500
-                touch:opacity-100 opacity-0 group-hover:opacity-100"
+                opacity-0 touch:opacity-100 group-hover:opacity-100"
                  href={link} target="_blank" rel="noreferrer">
                 <h2>{title} &#10230;</h2>
-                <p className="text-gray-400 mt-4">{desc}</p>
+                <p className="text-gray-500 dark:text-gray-400 mt-4">{desc}</p>
             </a>
         </article>
     )
