feacbe52e5556f4108c688573e6087478bb76855
lifecodeof <m.semihkoyuncu01@gmail.com>
2022-09-19 19:38:27 +0300

    readme & deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+pushd $(dirname $(realpath $0)) > /dev/null
+
+yarn run build
+touch out/.nojekyll
+
+DIR=$(mktemp -d)
+mv out $DIR
+
 git checkout production
-git merge --no-edit --no-ff main
+mv $DIR/out .
+git add .
+git commit -m "auto deployement"
 git push
 git checkout -
+
+popd > /dev/null
