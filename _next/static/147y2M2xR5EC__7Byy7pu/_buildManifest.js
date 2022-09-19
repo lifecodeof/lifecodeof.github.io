856b6c6009fe2c29412947d34c83f5f38809b6ec
lifecodeof <m.semihkoyuncu01@gmail.com>
2022-09-19 21:14:09 +0300

    auto deployement

---
+++ _next/static/147y2M2xR5EC__7Byy7pu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6751d6e3b1d56059.css","static/chunks/pages/index-a5fc92199a46b2d5.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
